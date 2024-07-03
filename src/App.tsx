b9a65ad0cd5bd4216b13eca5a87169e62bbf8abd
cosmicdaisy <haramn518@gmail.com>
2024-07-03 13:05:20 +0500

    badges and card added

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 import { Route, Routes } from "react-router-dom";
 import './App.css';
+import NavBar from "./components/ui/nav-bar";
 import HomePage from "./pages/home-page";
 import RecipePage from "./pages/recipe-page";
 
 function App() {
   return (
     <>
+      <NavBar />
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/recipes/:recipeId" element={<RecipePage/>} />
-
-
+        <Route path="/recipes/:recipeId" element={<RecipePage />} />
+        <Route path="*" element={<p> This page does not exist. </p>} />
       </Routes>
     </>
   )
---
+++ src/components/ui/nav-bar.tsx
@@ -0,0 +1,5 @@
+export default function NavBar() {
+    return (
+        <nav className="w-full border-0 px-10 py-4 lg:px-24 bg-gray-950"> <h1 className="text-3xl text-white"> <a href="/"> Tasty Reacts</a> </h1> </nav>
+    )
+}
